a1d5c9839d00ddede3c8237b200d84cb85c07ab1
YounesDjelloul <younesdjelloul14@gmail.com>
2025-05-07 00:57:17 +0800

    registering the middleware

--- TradeShield/settings.py
+++ TradeShield/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'authentication.middleware.TokenRefreshMiddleware',
 ]
 
 ROOT_URLCONF = 'TradeShield.urls'
