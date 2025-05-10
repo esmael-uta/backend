b00061fef0ef008078a34902f5b66367ac52af26
Esmael <esmaeluta@gmail.com>
2025-05-10 23:56:57 +0800

    add route for supply chain routes API in urls.py

--- TradeShield/urls.py
+++ TradeShield/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import get_supply_chain_routes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
+    path('api/supply-chain-routes/', get_supply_chain_routes, name='supply-chain-routes'),
 ]
